6ff7d1ee042f43bb643229715c2cfc7c130d18c2
akanoyebodepillar <akanoyebodepillar@gmail.com>
2024-07-05 22:28:13 -0400

    Update instance.tf
    removing ec2

--- instance.tf
+++ instance.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-west-2"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0604d81f2fd264c7b"
-  instance_type = "t2.micro"
-
-  tags = {
-    Name = "example-instance"
-  }
-}
+#resource "aws_instance" "example" {
+#  ami           = "ami-0604d81f2fd264c7b"
+#  instance_type = "t2.micro"
+#
+#  tags = {
+#    Name = "example-instance"
+#  }
+#}
